8d85bddce6341a7d5604e2de6f408e597f86ae87
marcelo saraiva <marcelossaraiva8@gmail.com>
2023-05-25 19:31:13 -0300

    001

---
+++ src/services/data/projetos.js
@@ -0,0 +1,27 @@
+export const Projetos = [
+  {
+    nome: 'Sistema Login(JS)', img: 'https://user-images.githubusercontent.com/122646917/240974197-4a523fbe-e3ca-4373-97e2-ad5b3105f610.png'
+    , github: 'https://github.com/MarceloSSaraivaa/Login_js'
+  },
+  {
+    nome: 'App lista de tarefas', img: 'https://user-images.githubusercontent.com/122646917/240974214-2103ca78-4096-4719-8069-9e5ca4255222.png'
+    ,  github: 'https://github.com/MarceloSSaraivaa'
+  },  
+  {
+    nome: 'Plataforma de blog', img: 'https://user-images.githubusercontent.com/122646917/240974214-2103ca78-4096-4719-8069-9e5ca4255222.png'
+    , github: 'https://github.com/MarceloSSaraivaa'
+  },
+  {
+    nome: 'App de clima', img: 'https://user-images.githubusercontent.com/122646917/240974214-2103ca78-4096-4719-8069-9e5ca4255222.png'
+    , github: 'https://github.com/MarceloSSaraivaa'
+  },
+  {
+    nome: 'Rede social', img: 'https://user-images.githubusercontent.com/122646917/240974214-2103ca78-4096-4719-8069-9e5ca4255222.png'
+    , github: 'https://github.com/MarceloSSaraivaa'
+  },
+  {
+    nome: 'E-commerce', img: 'https://user-images.githubusercontent.com/122646917/240974214-2103ca78-4096-4719-8069-9e5ca4255222.png'
+    , github: 'https://github.com/MarceloSSaraivaa'
+  },
+
+]
---
+++ src/services/data/skill.js
@@ -0,0 +1,30 @@
+import { DiHtml5,DiCss3Full,DiJsBadge,DiReact,DiDatabase,DiCode,DiBootstrap } from "react-icons/di";
+export const SkillsItem = [
+  {
+    titulo: 'HTML', Percentual: '65%', icon: <DiHtml5 size={35} color="#fff" />
+  },
+  {
+    titulo: 'CSS', Percentual: '60%', icon: <DiCss3Full size={35} color="#fff" />
+  },
+  {
+    titulo: 'Java Script', Percentual: '60%', icon: <DiJsBadge size={35} color="#fff" />
+  },
+  {
+    titulo: 'ReactJS', Percentual: '45%', icon: <DiReact size={35} color="#fff" />
+  },
+  {
+    titulo: 'ASPNet', Percentual: '70%', icon: <DiCode size={35} color="#fff" />
+  }
+  ,
+  {
+    titulo: 'Bootstrap', Percentual: '50%' ,  icon: <DiBootstrap size={35} color="#fff" />
+  }
+  ,
+  {
+    titulo: 'VBNet', Percentual: '85%', icon: <DiCode size={35} color="#fff" />
+  }
+  ,
+  {
+    titulo: 'SQLServer', Percentual: '65%' ,  icon: <DiDatabase size={35} color="#fff" />
+  }
+]
